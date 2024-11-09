5b231e212b30873b2eefad5993a2dac03e933ee0
bracheopterix <bracheopterix@gmail.com>
2024-11-10 00:45:35 +0200

    shifting

--- palindrom/pal.js
+++ palindrom/pal.js
@@ -1,26 +1,58 @@
 /* 
-we have a string like 'sldjdslfjhdaflsdfkhdsfhjdsfhds'
-we need to check if first half equals the last half in backwards order 
-so we take arraylength/2 - its ok if it not even, the last char would be just an axis of symmetry
+    we have a string like 'sldjdslfjhdaflsdfkhdsfhjdsfhds'
+    we need to check if first half equals the last half in backwards order 
+    so we take arraylength/2 - its ok if it not even, the last char would be just an axis of symmetry
 
-so we break an array on two halves (minus the axis-char) 
-we reverse the second array
-we feed two array to FOR and iterate by index
+    so we break an array on two halves (minus the axis-char) 
++   we reverse the second array  
+    we feed two array to FOR and iterate by index
  */
 
 
-
+// functions
 function reverseArr(array) {
     const result = array.reverse();
     return result;
-    // expecting result [1,2,3] to [3,2,1]
+    // expecting changing item order: [1,2,3] to [3,2,1]
 }
+
+function compareArr(arr1, arr2) {
+    let arrLength = arr1.length;
+    let counter = 0;
+    for (let i = 0; i < arr1; i++) {
+        if (arr1[i] == arr2[i]) {
+            counter = counter + 1;
+        };
+        else {return false};
+        
+        if (arrLength == counter) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+
+// export
+module.exports = {
+    reverseArr,
+    compareArr
+}
+
+// app
 
 const arr = [1, 2, 3];
 reverseArr(arr);
+compareArr(arr,arr);
 
-module.exports = {
-    reverseArr
-}
+
+
+
+
+
+
+
 
 
--- palindrom/pal.test.js
+++ palindrom/pal.test.js
@@ -1,4 +1,4 @@
-const { reverseArr } = require('./pal');
+const { reverseArr, compareArr } = require('./pal');
 
 
 test('this mf reversing an array order, so [1,2,3] becoming [3,2,1]', () => {
@@ -7,13 +7,24 @@ test('this mf reversing an array order, so [1,2,3] becoming [3,2,1]', () => {
     let superArr = reverseArr(protoArr);
     const expectArr = [3, 2, 1];
 
-    for (let i = 0; i < superArr; i++) {
-        superArr[i] = expectArr[i];
-        console.log(true);
+    for (let i = 0; i < superArr.length; i++) {
+        expect(superArr[i]).toBe(expectArr[i]);
+
+        // superArr[i] == expectArr[i];
     }
-
-
-
     // reverseArr([1,2,3]).toBe([3,2,1]);    
     // two objects instances would never be equal
 })
+
+
+test('this mf compare two arrays and returns true if they are the same', () => {
+const arr1 = [2,3,4];
+const arr2 = [2,3,4];
+const arr3 = [3,4,5];
+
+expect(compareArr(arr1,arr2)).toBe(true);
+expect(compareArr(arr1,arr3)).toBe(false);
+})
+
+
+
