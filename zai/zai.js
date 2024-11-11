47c003ea27fb1e8513aa7d355f7bdc4156240ba2
bracheopterix <bracheopterix@gmail.com>
2024-11-11 21:30:27 +0200

    started peach feature

---
+++ zai/zai.js
@@ -0,0 +1,4 @@
+// we calculate a timer that adds every .1 sec a 5% to a progress bar counter
+// we stretch an inner line in a progress bar
+// on every 20% there is a heart that glows up red (just change an icon)
+// the last heart turns uo in a peach
