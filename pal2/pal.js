936ebd98cb1ecb07c2a5740287b2f60ac78cc34f
bracheopterix <bracheopterix@gmail.com>
2024-11-17 19:03:36 +0200

    zaiaz in da haus

--- pal2/pal.js
+++ pal2/pal.js
@@ -1,62 +1,81 @@
-/*  Situation:
-    we have a string like 'sldjdslfjhdaflsdfkhdsfhjdsfhds'
-    we need to check if first half equals the last half in backwards order 
+// /*  Situation:
+//     we have a string like 'sldjdslfjhdaflsdfkhdsfhjdsfhds'
+//     we need to check if first half equals the last half in backwards order 
 
-    Steps:
-o   so we take arraylength/2 - its ok if it not even, the last char would be just an axis of symmetry
-o   so we break an array on two halfes (minus the axis-char) 
-0   we reverse the second array  
-0   we feed two array to FOR and iterate by index
+//     Steps:
+// o   so we take arraylength/2 - its ok if it not even, the last char would be just an axis of symmetry
+// o   so we break an array on two halfes (minus the axis-char) 
+// 0   we reverse the second array  
+// 0   we feed two array to FOR and iterate by index
 
- */
+//  */
 
 
-// functions
+// // functions
 
-function checkEven(array) {
-    let result;
-    if (array.length % 2 == 0) {
-        result = true;
+// function checkEven(array) {
+//     let result;
+//     if (array.length % 2 == 0) {
+//         result = true;
+//     }
+//     else {
+//         result = false;
+//     }
+//     return result;
+// }
+
+// function cutArray(array) {
+//     const clonedArray = array;
+//     let half = Math.floor(array.length / 2);
+//     // console.log(half);
+//     const subArr1 = clonedArray.splice(0, half)
+//     // console.log(clonedArray);
+//     let subArr2 = clonedArray;    
+//     if (checkEven(array) == true) {
+//         // console.log("true");
+//         subArr2.shift();
+//     }
+//     else {
+//         // console.log("false");
+//     }
+//     const result = [subArr1, subArr2];
+//     console.log(result);
+//     return result;
+
+// }
+
+
+// console.log(checkEven([1, 2, 3, 'a', 'i']));
+// cutArray([1, 2, 3, 'a', 'i']);
+
+// console.log(checkEven([1, 2, 3, 'z', 'a', 'i']));
+// cutArray([1, 2, 3, 'z', 'a', 'i']);
+
+// // var leftSide = arrayName.splice(0, Math.floor(arrayName.length / 2));
+
+// // cutArray([1, 2, 3, 4, 5]);
+
+
+
+
+
+function palindrome(s) {
+    const sLongth = s.length - 1;
+    console.log("slongth = ", sLongth);
+    for (let i = 0; i < s.length/2;i++) {
+        if (s[i] == s[sLongth-i]) {
+            console.log(`s ${i} = ${s[i]} vs s${sLongth-i} = ${s[sLongth-i]}`);
+        }
+        else{
+            return false;
+        }
     }
-    else {
-        result = false;
-    }
-    return result;
+    return true;
 }
-
-function cutArray(array) {
-    const clonedArray = array;
-    let half = Math.floor(array.length / 2);
-    // console.log(half);
-    const subArr1 = clonedArray.splice(0, half)
-    // console.log(clonedArray);
-    let subArr2 = clonedArray;    
-    if (checkEven(array) == true) {
-        console.log("true");
-        subArr2.shift();
-    }
-    else {
-        console.log("false");
-    }
-    const result = [subArr1, subArr2];
-    // console.log(result);
-    return result;
-
-}
-
-
-console.log(checkEven([1, 2, 3, 'a', 'i']));
-cutArray([1, 2, 3, 'a', 'i']);
-
-console.log(checkEven([1, 2, 3, 'z', 'a', 'i']));
-cutArray([1, 2, 3, 'z', 'a', 'i']);
-
-// var leftSide = arrayName.splice(0, Math.floor(arrayName.length / 2));
-
-// cutArray([1, 2, 3, 4, 5]);
 
 
 module.exports = {
-    checkEven,
-    cutArray
+    // checkEven,
+    // cutArray,
+    palindrome
 }
--- pal2/pal.test.js
+++ pal2/pal.test.js
@@ -1,16 +1,31 @@
-const { checkEven, cutArray } = require('./pal');
+// const { checkEven, cutArray } = require('./pal');
+
+const { palindrome } = require('./pal');
+
+// const arr1 = [1, 2, 3, 'z', 'a', 'i'];
+// const arr2 = arr1;
+// const arr3 = ['i', 'a', 'z', 3, 2, 1]
+
+// test('checkEven - if the array have even number of letters', () => {
+//     expect(checkEven(arr1)).toBe(true);
+//     expect(checkEven(arr1.pop())).toBe(false);
+//     console.log(arr1,checkEven(arr1));
+// })
+
+// test ('cutArrat - if two parts of array are normal',()=>{
+// expect(cutArray(arr1)).toBe([[1,2,3],['z', 'a', 'i']])
+// })
 
 
-const arr1 = [1, 2, 3, 'z', 'a', 'i'];
-const arr2 = arr1;
-const arr3 = ['i', 'a', 'z', 3, 2, 1]
-
-test('checkEven - if the array have even number of letters', () => {
-    expect(checkEven(arr1)).toBe(true);
-    expect(checkEven(arr1.pop())).toBe(false);
-    console.log(arr1,checkEven(arr1));
+test('palindrome test checks palindrome expects false', () => {
+    expect(palindrome('abca')).toBe(false);
 })
 
-test ('cutArrat - if two parts of array are normal',()=>{
-expect(cutArray(arr1)).toBe([[1,2,3],['z', 'a', 'i']])
+
+test('palindrome test checks palindrome expects true', () => {
+    expect(palindrome('zaiiaz')).toBe(true);
+})
+
+test('pal test checks pal with not even number of chars',()=>{
+    expect(palindrome('zaiziaz')).toBe(true);
 })
