e29d26b13a39f2bdd0498aab9b0f967d22d51cff
bracheopterix <bracheopterix@gmail.com>
2024-11-18 22:00:04 +0200

    zai animation started

--- zai/zai.js
+++ zai/zai.js
@@ -2,3 +2,10 @@
 // we stretch an inner line in a progress bar
 // on every 20% there is a heart that glows up red (just change an icon)
 // the last heart turns uo in a peach
+
+
+// function fullfilling (){
+//     let progressLine = document.getElementById('progress-line');
+    
+//     progressLine.style.width = '500px';
+// }
