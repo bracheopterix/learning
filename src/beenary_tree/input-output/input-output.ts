16bf781ae88f097b0d8b37e97ca056d0fb0b9f51
bracheopterix <bracheopterix@gmail.com>
2025-02-11 15:49:52 +0200

    cleaned a little

--- src/beenary_tree/input-output/input-output.ts
+++ src/beenary_tree/input-output/input-output.ts
@@ -2,27 +2,51 @@
 
 type T = null | boolean | number;
 
+let data:T = false;
 
-function input (argument:T):T {
-    const type = typeof argument;
-    let result:T = false;
-    switch (type) {
-        case 'object':
-            result = -1;
-            break;
-        case 'boolean':
-            if(type){
-                result = -5;
-            }
-            else{
-                result = -10;
-            }
-            break;
-        case 'number':
-            result = -100;
-            break;
-    }
-    return result;
+const type = typeof data;
+let result:T = false;
+switch (type) {
+    case 'object':
+        result = -1;
+        break;
+    case 'boolean':
+        if(data){
+            result = -5;
+        }
+        else{
+            result = -10;
+        }
+        break;
+    case 'number':
+        result = Math
+
+
+
+        break;
 }
+console.log(result);
 
-console.log(input(3));
+// function input (data:T):T {
+//     const type = typeof data;
+//     let result:T = false;
+//     switch (type) {
+//         case 'object':
+//             result = -1;
+//             break;
+//         case 'boolean':
+//             if(type){
+//                 result = -5;
+//             }
+//             else{
+//                 result = -10;
+//             }
+//             break;
+//         case 'number':
+//             result = -100;
+//             break;
+//     }
+//     return result;
+// }
+
+// console.log(input(3));
